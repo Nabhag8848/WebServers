cfd51fd780b80c5ea37e0bdca59887e90735096a
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-01-24 22:59:34 +0530

    Learnt about render dynamic pages with hbs and removed html files
    Learnt about render dynamic pages with hbs and removed html files

--- src/app.js
+++ src/app.js
@@ -4,7 +4,31 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
 
+
+app.set('view engine','hbs')
+app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(publicDirectoryPath))
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        object:'This is Dynamic'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Me',
+        name:'Nabhag Motivaras'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        message:'We Grow by Lifting others!!'
+    })
+})
+
 
 app.get('/weather',(req,res)=>{
     res.send({
