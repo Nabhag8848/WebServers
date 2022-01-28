4bc5fa8ebf491e11a43d8bd1f3084183ce59731d
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-01-28 12:08:20 +0530

    Learnt about: Fetch, Creating form and use the data we get from input to get some Info(here Forecast) and render that back too clientside.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,33 @@
 console.log('Javascript is loaded!')
+
+// fetch('https://puzzle.mead.io/puzzle').then((response)=>{
+
+//     response.json().then((data)=>{
+
+//         console.log(data)
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const location = searchElement.value
+    messageOne.textContent = 'Loading......'
+    messageTwo.textContent = '' 
+    fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+        response.json().then((data)=>{
+
+            if(data.error){
+                return messageOne.textContent = data.error
+            }
+
+            messageOne.textContent = data.forecast
+            messageTwo.textContent = data.location
+        })
+    })
+})
